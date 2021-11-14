287a0ad2a7b8dd385e4429674f60aca2892f3cf6
patricoder <p.siechodnik@gmail.com>
2021-11-14 20:47:48 +0100

    Add some styles to order section

--- src/Components/LoggedIn/Orders.js
+++ src/Components/LoggedIn/Orders.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
-
 import {OrderContext, UserContext} from "../Context/Context";
 import "../../SCSS/Order.scss"
+import OrdersForm from "./OrdersForm";
 
 const Orders = () => {
     const {user, order, setOrder, cars} = useContext(UserContext);
@@ -13,70 +13,34 @@ const Orders = () => {
             }
         })
     }
-    // const changeCar = (e) => {
-    //     setOrder(prev => {
-    //         return {
-    //             ...prev,
-    //             car: {
-    //                 ...prev.car,
-    //                 [e.target.name]: e.target.value
-    //             }
-    //         }
-    //     })
-    // }
     const addDataToFirestore = async () => {
         // let data = await addDoc(collection(db, "orders"),{});
         let orderInfo = {
             email: user.email,
             name: order.name,
             secondName: order.secondName,
-            Age: order.Age,
-            price: order.howLong >= 30 ? (order.price * order.howLong)-((order.price * order.howLong)*0.3) : order.price * order.howLong,
-            howLong:+order.howLong,
+            price: order.howLong >= 30 ? (order.price * order.howLong) - ((order.price * order.howLong) * 0.3) : order.price * order.howLong,
+            howLong: +order.howLong,
             city: order.city,
             car: order.car,
-            startingDate: order.startingDate,
+            startingDate: order.startingDate
         }
         console.log(orderInfo)
     }
     const sendOrder = (e) => {
         e.preventDefault();
-        addDataToFirestore();
+        if (order.name === '' || order.secondName === '' || order.price === false || order.howLong === "" || order.city === '' || order.startingDate === '' || order.car === "main") {
+            alert('Uzupełnij wszystkie pola');
+        } else {
+            addDataToFirestore();
+        }
     }
-    return (<OrderContext.Provider value={{order, setOrder}}>
-            {order.car}
-            <div className={"orderform"}>
-                <form onSubmit={sendOrder}>
-                    <input type={"text"} name={"email"} value={order.email} onChange={changeState}/>
 
-                   <select name={"car"} onChange={changeState} value={order.car}>
-                       <option value={'main'}>choose a car...</option>
-                       {cars.map(car=>{
-                           return <option value={`${car.brand} ${car.model}`}>{car.brand} {car.model}</option>
-                       })}BMW
-
-                   </select>
-                    Zł/Day
-                    <input type={"string"} value={+order.price} placeholder={"Price for a day"}
-                           onChange={changeState}/>
-                    {order.howLong >= 30 && <p>Wynajem powyżej 30 dni? 30% rabatu</p>}
-
-                    <input type={"text"} name={"name"} value={order.name} placeholder={"Name"} onChange={changeState}/>
-                    <input type={"text"} name={"secondName"} value={order.secondName} placeholder={"Second name"}
-                           onChange={changeState}/>
-                    <input type={"number"} name={"Age"} value={order.Age} placeholder={"Age"} min={18}
-                           onChange={changeState}/>
-
-                    <input type={"text"} name={"city"} value={order.city} placeholder={"City"} onChange={changeState}/>
-
-                    FROM<input type={"date"} value={order.startingDate} name={"startingDate"} onChange={changeState}/>
-                    HOW LONG <input type={"number"} value={order.howLong} name={"howLong"} onChange={changeState}/>
-                    <button type={"submit"}>Confirm</button>
-
-                </form>
-            </div>
-        </OrderContext.Provider>
-    );
-};
-
+return <OrderContext.Provider value={{order, setOrder}}>
+    <div className={"orderform"}>
+        <OrdersForm order={order} cars={cars} changeState={changeState} sendOrder={sendOrder}/>
+    </div>
+</OrderContext.Provider>
+}
 export default Orders;
+
---
+++ src/Components/LoggedIn/OrdersForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "../../SCSS/Order.scss"
+const OrdersForm = ({cars,order, sendOrder,changeState}) => {
+    return <form className={"orderform__inputs"} onSubmit={sendOrder}>
+        <input type={"text"} name={"email"} value={order.email} onChange={changeState}/>
+
+        <select name={"car"} onChange={changeState} value={order.car}>
+            <option value={'main'}>chose a car...</option>
+            {cars.map(car => {
+                return <option value={`${car.brand} ${car.model}`}>{car.brand} {car.model}</option>
+            })}BMW
+
+        </select>
+
+        {order.price && <><p>PLN/DAY</p>
+            <input type={"string"} value={+order.price} placeholder={"Price for a day"}
+                   onChange={changeState}/>
+            {order.howLong >= 30 && <p className={"discount"}>for rentals over 30 days, 30% discount</p>}</>}
+
+        <input type={"text"} name={"name"} value={order.name} placeholder={"Name"} onChange={changeState}/>
+        <input type={"text"} name={"secondName"} value={order.secondName} placeholder={"Second name"}
+               onChange={changeState}/>
+
+        <input type={"text"} name={"city"} value={order.city} placeholder={"City"} onChange={changeState}/>
+
+        FROM<input type={"date"} value={order.startingDate} name={"startingDate"} onChange={changeState}/>
+        HOW LONG <input type={"number"} value={order.howLong} name={"howLong"} onChange={changeState}/>
+        <button type={"submit"}>Confirm</button>
+
+    </form>
+};
+
+export default OrdersForm;
